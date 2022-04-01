9aefd8b8a6f0708b1e7c467c8671b761dd3044ed
Zhenye Zhu <67647342+ZhenyeZhu@users.noreply.github.com>
2022-04-01 13:40:12 -0400

    fix unused

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -1,17 +1,15 @@
 import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider as Provider,
-    useQuery,
-    gql
-  } from "@apollo/client";
-import React from 'react';
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider as Provider,
+} from "@apollo/client";
+import React from "react";
 
 const client = new ApolloClient({
-    uri: 'https://chat-app-apollo.herokuapp.com/',
-    cache: new InMemoryCache()
+  uri: "https://chat-app-apollo.herokuapp.com/",
+  cache: new InMemoryCache(),
 });
 
 export default function ApolloProvider(props) {
-    return <Provider client={client} {...props} />
+  return <Provider client={client} {...props} />;
 }
--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
-import logo from "./logo.svg";
 import "./App.scss";
 import React from "react";
-import { Container} from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
-
-import ApolloProvider from './ApolloProvider';
-
-
+import ApolloProvider from "./ApolloProvider";
 
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 
-
-
-function App(){
+function App() {
   return (
     <ApolloProvider>
       <BrowserRouter>
         <Container className="pt-5">
           <Routes>
-            <Route exact path="/" element= {<Register />} />
-            <Route path="/home" element= {<Home />} />
-            <Route path="/login" element= { <Login />} />
+            <Route exact path="/" element={<Register />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
-          
         </Container>
       </BrowserRouter>
-    </ApolloProvider >
+    </ApolloProvider>
   );
 }
 
